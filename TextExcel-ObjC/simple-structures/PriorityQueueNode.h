4e8868a8384c21a381024194d89578678395d97f
triscuitcircuit <54428989+Triscuit-circuit@users.noreply.github.com>
2023-05-11 20:25:14 -0400

    added PriorityQueueNodes

---
+++ TextExcel-ObjC/simple-structures/PriorityQueueNode.h
@@ -0,0 +1,15 @@
+//
+// Created by Tristan Zippert on 5/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PriorityQueueNode: NSObject
+
+@property(nonatomic, strong) id object;
+@property (nonatomic, assign) NSInteger priority;
+@property (nonatomic, strong)PriorityQueueNode *next;
+
+-(instancetype)initWithType:(id)object priority:(NSInteger)priority;
+@end
---
+++ TextExcel-ObjC/simple-structures/PriorityQueueNode.m
@@ -0,0 +1,21 @@
+//
+// Created by Tristan Zippert on 5/11/23.
+//
+
+#import "PriorityQueueNode.h"
+
+@implementation PriorityQueueNode : NSObject
+
+
+
+- (nonnull instancetype)initWithType:(nonnull id)object  priority:(NSInteger)priority {
+    self = [super init];
+    if (self){
+        _object = object;
+        _priority = priority;
+        _next = nil;
+    }
+    return self;
+}
+
+@end
